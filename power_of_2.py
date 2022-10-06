1871045d43712423e69e86e167afaf6e924cf14b
Milan Biranwar <milanbiranwar06@gmail.com>
2022-10-06 12:35:32 +0530

    Program for power of 2

---
+++ power_of_2.py
@@ -0,0 +1,11 @@
+# Program for power of 2
+
+def power_of_2():
+    num = 1
+    number = int(input("Enter the number"))
+    for i in range(number):
+        num = num * 2
+    print(num)
+
+
+power_of_2()
