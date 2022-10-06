cf0bb34462a341f562a72d7460ccf4061a19ebed
Milan Biranwar <milanbiranwar06@gmail.com>
2022-10-06 12:49:03 +0530

    Program for number swaping

---
+++ swap_two_number.py
@@ -0,0 +1,12 @@
+def swap_two_num():
+    a = int(input("Enter number to swap"))
+    b = int(input("Enter number to swap with another number"))
+    c = a
+    x = b
+    y = c
+    print("Before swaping value of a is ", a)
+    print("Before swaping value of b is ", b)
+    print("After swaping value of a is ", x)
+    print("After swaping value of b is ", y)
+
+swap_two_num()
