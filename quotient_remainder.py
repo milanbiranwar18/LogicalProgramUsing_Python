df14c086d416befc463089506f64c2da04d7ef97
Milan Biranwar <milanbiranwar06@gmail.com>
2022-10-06 12:45:45 +0530

    Program for quotient remainder

---
+++ quotient_remainder.py
@@ -0,0 +1,10 @@
+def quotient_remainder():
+    num1 = int(input("Enter divident number"))
+    num2 = int(input("Enter divisor number"))
+    quotient = num1 / num2
+    remainder = num1 % num2
+    print("Quotient is", quotient)
+    print("Remainder is", remainder)
+
+
+quotient_remainder()
